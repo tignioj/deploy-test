550813000abb537e8d57dc2601f6c414a4cc218a
tignioj <859897994@qq.com>
2024-09-27 04:40:18 +0800

    结构调整

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -11,6 +10,25 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static/assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'static/assets/[name].[hash].js',
+        chunkFileNames: 'static/assets/[name].[hash].js',
+        assetFileNames: 'static/assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  experimental: {
+    renderBuiltUrl(filename, { hostType }) {
+      if (hostType === 'html') {
+        return { relative: true }
+      }
+      return { relative: true }
     }
   }
 })
