544ab6e866bd6fd5fb26fc523c58b87f410e6041
tignioj <859897994@qq.com>
2024-09-27 14:27:36 +0800

    结构调整

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import flaskPlugin from "./vite-plugin-flask.js";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'static/assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'static/assets/[name].[hash].js',
-        chunkFileNames: 'static/assets/[name].[hash].js',
-        assetFileNames: 'static/assets/[name].[hash].[ext]'
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      // 仅在 build 模式下启用 flaskPlugin
+      command === 'build' && flaskPlugin()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
-  },
-  experimental: {
-    renderBuiltUrl(filename, { hostType }) {
-      if (hostType === 'html') {
-        return { relative: true }
-      }
-      return { relative: true }
-    }
-  }
-})
+    },
+  };
+});
